bd03f49db94460bc620e808f19f2cefe4c3a4bea
Mikhail Kalinin <new-mishut@mail.ru>
2023-03-12 17:56:04 +0300

    update templates

--- app/Views/404.php
+++ app/Views/404.php
@@ -1,18 +1,14 @@
-<?php
-
-echo "
-<html lang=\"en\">
+<html lang="en">
 <head>
-<title>App!</title>
+    <title>App!</title>
 </head>
 <body>
 <table>
-<tr>
-<td>
-404
-</td>
-</tr>
+    <tr>
+        <td>
+            404
+        </td>
+    </tr>
 </table>
 </body>
 </html>
-";
